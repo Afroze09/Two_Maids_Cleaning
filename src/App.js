574496d07b391feffdd478537758a46f7db3d7c5
“Yuvatheja” <skafroze2002@gmail.com>
2023-11-03 16:20:05 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
+import React from 'react'
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
