0edfe0d9027746313a76771c3155ecbfbcad5762
Rutzz <rutikabhang@gmail.com>
2023-11-03 16:20:52 +0530

    hiu

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../index.css'
+const Footer = () => {
+    return (
+        <div>
+            <img className='h-25 w-25 mt-5' src="https://res.cloudinary.com/twomaidsengi/image/upload/c_scale,f_auto,q_auto,w_2000/v1670273202/hubsite-reskin/let-life-shine-pink.webp" alt="" />
+        </div>
+    );
+}
+
+export default Footer;
