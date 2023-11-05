adc5ec7069529dffcac6347771afdc9ce9f0655b
“Yuvatheja” <skafroze2002@gmail.com>
2023-11-06 01:35:06 +0530

    aboutus

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,16 @@ export const aboutUs = [
         "Our company also supports the “Cleaning for a Reason” organization. Through this relationship, we offer free house cleanings to women undergoing cancer treatment. Our family has known cancer first-hand and how the support and kindness of others can be a blessing.",
         " Our company also supports the “Cleaning for a Reason” organization. Through this relationship, we offer free house cleanings to women undergoing cancer treatment. Our family has known cancer first-hand and how the support and kindness of others can be a blessing."
 ]
+
+export const chesterfield=[
+        'Hi, we are Jordan and Carly Black, and are the proud owners and leaders of the maid service team at Two Maids & A Mop serving Chesterfield, MO!',
+        "I consider myself a late bloomer in the business world, as I was first a music teacher for 10 years. However, the spirit of entrepreneurship has always been with me. I enjoyed teaching, however, I was always thinking of new business ideas and taking on side ventures. After founding and directing multiple music groups and a local musician’s network, I knew the business side of me was longing for my 100% commitment.",
+        "Carly’s joy of business comes from building relationships and helping others. Carly’s positive energy makes her someone easy to connect with and enjoy being around. Her role in Two Maids includes being a smiling, welcoming presence in our community, for both customers and maid service team. Additionally, Carly works with her mother and sister in a business called Revival Society where they help entrepreneurial moms get free from the overwhelming role of motherhood.",
+        "Carly and I have always felt passionate about having a clean and organized home. We believe that this can truly bring positive energy to a family. So, when we started to pursue business opportunities, we felt deeply connected to the residential cleaning industry. With our employee incentive payment plan called Pay for Performance, a family-like atmosphere with our wonderful employees and clients, as well as the relationship with Cleaning for a Reason, we knew Two Maids & A Mop was a perfect match.",
+        "It is because of all of this, that as the local and proud owners of Two Maids & A Mop of Chesterfield, that we will give our customers the precious gift of time as well as continual positive energy through the Two Maids Cleaning Experience.",
+        "Discover why we are a “Maid Service worth talking about!”"
+]
+
 export const corouselImg = [
         "https://res.cloudinary.com/twomaidsengi/image/upload/c_fill,g_south,h_900,q_auto:eco,w_1920/v1696521709/hubsite-reskin/slider-image-1.webp",
         "https://res.cloudinary.com/twomaidsengi/image/upload/c_fill,g_south,h_900,q_auto:eco,w_1920/v1696521709/hubsite-reskin/slider-image-2.webp",
