4f51bc01437ab8643ad97f9a927aec2d28e0bedd
“Yuvatheja” <skafroze2002@gmail.com>
2023-11-07 01:37:14 +0530

    rooms

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,34 +1,29 @@
-import React, { useState } from "react";
-import Navbar from "../Components/Navbar";
-import { Footer } from "flowbite-react";
-import { data, title } from "../data";
+import React from "react";
 import OneTime from "../Components/OneTime";
-import { Collapse } from "react-bootstrap";
-import Rooms from "../Components/Rooms";
-import { serviceinfo} from "../data"
+import { serviceinfo } from "../data";
+
 const Services = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-      };
-      
-    return (
-        <>
-            <div className='h-full w-full'>
-            <div className='main-info h-[100vh] w-full p-3' style={{backgroundImage:'url("https://twomaidscleaning.com/_next/image/?url=https%3A%2F%2Fres.cloudinary.com%2Ftwomaidsengi%2Fimage%2Fupload%2Fc_scale%2Cq_44%2Cw_2515%2Fv1670256609%2Fhubsite-reskin%2FTwoMaids_3.jpg&w=3840&q=75")',backgroundPosition:'center',backgroundSize:'cover'}}>
-                <div className='ml-20 mt-40 w-[520px]'>
-                <h1 className="text-6xl font-bold text-pink-600">Our Services</h1>
+  return (
+    <>
+      <div className="h-full w-full">
+        <div
+          className="main-info h-[100vh] w-full p-3"
+          style={{
+            backgroundImage:
+              'url("https://twomaidscleaning.com/_next/image/?url=https%3A%2F%2Fres.cloudinary.com%2Ftwomaidsengi%2Fimage%2Fupload%2Fc_scale%2Cq_44%2Cw_2515%2Fv1670256609%2Fhubsite-reskin%2FTwoMaids_3.jpg&w=3840&q=75")',
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="ml-20 mt-40 w-[520px]">
+            <h1 className="text-6xl font-bold text-pink-600">Our Services</h1>
+            {serviceinfo.map((info) => {
+              return (
                 <p className=" text-lg break-words mt-10 text-gray-500">
-                We’re proud to provide safe and reliable professional-grade cleaning supplies and products. Each of our teams of Two Maids has been professionally trained to deliver high quality house cleaning services. We offer multiple cleaning packages that are designed to meet the demands of any home owner. Whether you need your house cleaning services on a regular basis or just one time, we have a cleaning option that's right for you. A comprehensive description of each cleaning package is below.
+                  {info}
                 </p>
-                <div className='flex justify-evenly mt-16 -ml-5'>
-                    <button className='h-10 w-48 text-white font-bold justify-evenly bg-pink-600'>ONE-TIME-CLEAN</button>
-                    <button className='h-10 w-48 text-white font-bold justify-evenly bg-pink-600'>RECURRING CLEAN</button>
-                </div>
-                </div>
-                {/* <img className='h-[100%] w-full' src= alt="" /> */}
-                
-            </div>
+              );
+            })}
             <div className="flex justify-evenly mt-16 -ml-5">
               <button className="h-10 w-48 text-white font-bold justify-evenly bg-pink-600">
                 ONE-TIME-CLEAN
@@ -38,14 +33,21 @@ const Services = () => {
               </button>
             </div>
           </div>
+          {/* <img className='h-[100%] w-full' src= alt="" /> */}
+        </div>
+        <div className="flex justify-evenly mt-16 -ml-5">
+          <button className="h-10 w-48 text-white font-bold justify-evenly bg-pink-600">
+            ONE-TIME-CLEAN
+          </button>
+          <button className="h-10 w-48 text-white font-bold justify-evenly bg-pink-600">
+            RECURRING CLEAN
+          </button>
         </div>
       </div>
       <div className=" w-full">
         <OneTime />
       </div>
-      <div className=" w-full">
-        
-      </div>
+      <div className=" w-full"></div>
     </>
   );
 };
