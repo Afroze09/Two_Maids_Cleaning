02a40b9e3fa768e11e7ef26d59320671c1314151
“Yuvatheja” <skafroze2002@gmail.com>
2023-11-10 00:23:54 +0530

    complete responsive

--- src/Components/Ourservices.jsx
+++ src/Components/Ourservices.jsx
@@ -24,28 +24,28 @@ export default function Ourservices() {
   });
 
   return (
-    <section className="py-12 px-[250px] h-full w-screen ourservices mt-3">
+    <section className="py-12 lg:px-[250px] md:px-[60px] h-full w-screen ourservices mt-3">
       <div className="w-full mb-5">
-        <h1 className="sm:text-7xl text-5xl text-left font-bold font-[TiemposFine]  mb-5 text-[#015d9c] ">
-          <span className="sm:text-7xl text-5xl sm:inline hidden mb-5">
-            Your day just got a <br /> whole lot
+        <h1 className="sm:text-7xl lg:text-7xl text-5xl text-left font-bold font-[TiemposFine]  mb-5 text-[#015d9c] ">
+          <span className="sm:inline md:hidden lg:inline hidden mb-5">
+            Your day just got a <br /> whole lot 
           </span>
-          <span className="sm:hidden block mb-2 text-5xl">Your day just</span>
-          <span className="sm:hidden block mb-2  text-5xl">
+          <span className="sm:hidden md:block lg:hidden block mb-2 md:text-6xl text-5xl">Your day just</span>
+          <span className="sm:hidden md:block lg:hidden block mb-2 md:text-6xl text-5xl">
             got a whole lot
           </span>
-          <span className="sm:ml-4">{rotatingWords[currentWordIndex]}</span>.
+          <span className="sm:ml-4 lg:ml-4 lg:text-7xl  md:text-6xl md:ml-0">{rotatingWords[currentWordIndex]}</span>.
         </h1>
-        <p className="sm:text-[27px] text-lg text-left sm:tracking-[0.07em] text-[#7b6c73] leading-9">
+        <p className="sm:text-[27px] lg:text-[27px] md:text-lg text-lg text-left lg:tracking-[0.07em]  sm:tracking-[0.07em] text-[#7b6c73] leading-9">
           Let Two Maids do all of the dirty work for you. Focus on everything
           else you need to get done and schedule a cleaning with us today.
         </p>
       </div>
       <div className="w-full">
-        <h4 className="text-[#015d9c] font-bold mb-3 text-[25px]  sm:text-3xl">
+        <h4 className="text-[#015d9c] font-bold mb-3 md:text-[30px] text-[25px] lg:text-3xl   sm:text-3xl">
           Your Resident Home Cleaning Expert
         </h4>
-        <p className="sm:text-[16px] text-[15px] text-left text-[#5f6060] font-medium leading-7 mb-5">
+        <p className="sm:text-[16px] lg:text-[16px] md:text-[17px] text-[15px] text-left text-[#5f6060] font-medium leading-7 mb-5">
           At Two Maids, our purpose is to make a difference in people’s daily
           lives. We help busy families maintain beautiful, clean and healthy
           homes and take care of the chores that no one has the time or energy
@@ -54,12 +54,12 @@ export default function Ourservices() {
           do makes lives a little easier and homes a little brighter.
         </p>
         <div className="bg-[#a0dcff] sm:bg-transparent flex flex-col items-center justify-center py-4 sm:py-0">
-          <div className="sm:grid sm:grid-cols-3 md:grid md:grid-cols-3 flex flex-col items-center sm:gap-7 sm:h-[260px] md:gap-7 md:h-[260px] w-full mb-4">
+          <div className="sm:grid lg:grid sm:grid-cols-3 lg:grid-cols-3 md:grid md:grid-cols-3 flex flex-col items-center sm:gap-7 lg:gap-7 lg:h-[260px] sm:h-[260px] md:gap-x-6 md:h-[260px] w-full mb-4">
             {services.map((v) => {
               return (
                 <div
                   key={v.id}
-                  className="flex h-64 w-[240px] flex-col items-center"
+                  className="flex h-64 w-[240px] md:w-auto lg:w-[240px] flex-col items-center"
                 >
                   <span className="text-6xl text-[#015d9c]">{v.icon}</span>
                   <h4 className="text-center text-lg text-[#015d9c] font-bold ">
