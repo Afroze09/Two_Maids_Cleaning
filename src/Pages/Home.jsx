5ba1f6cedae5a2b90153befafc4a9ef66e351269
Rutzz <rutikabhang@gmail.com>
2023-11-11 22:26:58 +0530

    services

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className='bg-[#015d9c] h-full flex flex-col justify-evenly items-center px-2 py-10 sm:px-28 md:px-28 sm:py-20 md:py-20 text-white'>
         <div>
         <h1 className='sm:text-6xl text-4xl font-bold mb-3 tracking-widest leading-[1.2em] '>WELCOME TO THE <br /> KILLER CLEAN <br /> EXPERIENCE!</h1>
-        <p className='text-xl'>We are dirts worst nightmare</p>
+        <p className='text-xl'>Dirts worst nightmare.</p>
         </div>
         <Link to="/aboutus"><button className='btn event-btn mt-4 sm:mt-8' id='learn'>Learn More</button></Link>
       </div>
--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -3,30 +3,31 @@ import OneTime from "../Components/OneTime";
 import { serviceinfo} from "../data"
 import Recurring from "../Components/Recurring";
 import CommonQuestion from "../Components/CommonQuestion";
+import img from "../Images/service-bg-img.png"
 const Services = () => {
   return (
     <>
-      <div className=" w-screen ">
+      <div className=" h-full w-full ">
         <div
-          className="main-info md:flex h-full w-full md:p-1 p-3  md:bg-cover sm:bg-cover bg-contain lg:bg-cover bg-no-repeat  bg-top lg:bg-center md:bg-center sm:bg-center"
+          className="main-info  h-full w-full md:p-1 p-3  bg-center"
           style={{
             backgroundImage:
-              'url("https://twomaidscleaning.com/_next/image/?url=https%3A%2F%2Fres.cloudinary.com%2Ftwomaidsengi%2Fimage%2Fupload%2Fc_scale%2Cq_44%2Cw_2515%2Fv1670256609%2Fhubsite-reskin%2FTwoMaids_3.jpg&w=3840&q=75")'
+            "url(" + img + ")",
            
           }}
         >
-          <div className="sm:ml-4 ml-0 sm:mt-4 mt-[250px]  md:ml-2 lg:ml-4 flex flex-col">
-            <div className="lg:w-[520px]  md:w-[400px] sm:w-[250px] w-full">
-            <h1 className="text-3xl lg:text-6xl md:text-5xl  font-bold text-[#015d9c] ">Our Services</h1>
+          <div className="sm:ml-4 ml-0 sm:mt-4 mt-[250px]  md:ml-2 lg:ml-16 flex flex-col">
+            <div className="lg:w-[520px] mt-32 md:w-[400px] sm:w-[250px] w-full ">
+            <h1 className="text-3xl lg:text-6xl md:text-5xl tracking-[-0.07em] font-bold text-[#015d9c] font-[TiemposFine] ">Our Services</h1>
             {serviceinfo.map((info) => {
               return (
-                <p className="text-sm md:text-[15px] lg:text-lg break-words sm:mt-4 text-gray-500">
+                <p className="text-sm md:text-[15px] lg:text-lg break-words sm:mt-4 text-gray-200">
                   {info}
                 </p>
               );
             })}
             </div>
-            <div className="flex flex-col sm:flex-row  mt-4 md:-ml-4 sm:mt-16 ">
+            <div className="flex flex-col sm:flex-row  mt-4 lg:ml-0 md:-ml-4 sm:mt-16 ">
               <button className="h-10 p-2 rounded sm:mr-1 md:mr-1 text-white font-bold bg-[#015d9c] my-2 sm:my-0">
                 ONE-TIME-CLEAN
               </button>
